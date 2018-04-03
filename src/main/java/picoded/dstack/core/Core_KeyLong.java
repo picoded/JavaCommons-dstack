052471968541b8e834a9ac0c50f92f6b71635810
jiaming <jiaming@uilicious.com>
2018-04-03 15:26:23 +0800

    Added Core classes for KeyLong and KeyLongMap

---
+++ src/main/java/picoded/dstack/core/Core_KeyLong.java
@@ -0,0 +1,255 @@
+package picoded.dstack.core;
+
+import picoded.dstack.KeyLong;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Core_KeyLong implements KeyLong {
+
+	//--------------------------------------------------------------------------
+	//
+	// Core variables
+	//
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Core_KeyValueMap used for the object
+	 * Used to provide the underlying backend implementation
+	 **/
+	protected Core_KeyLongMap main = null;
+
+	/**
+	 * Current keyname used to identify the stored value
+	 */
+	protected String key = null;
+
+//	protected static ReentrantLock arithmeticLock = new ReentrantLock();
+//	protected static ReentrantLock expiryLock = new ReentrantLock();
+
+	//--------------------------------------------------------------------------
+	//
+	// Constructor
+	//
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Setup a Core_KeyLong against a Core_KeyLongMap backend.
+	 *
+	 * This allow the setup in the following modes
+	 *
+	 * @param  keyLongMap backend to use
+	 * @param  key used to identify the stored value
+	 */
+	public Core_KeyLong(Core_KeyLongMap keyLongMap, String inKey) {
+		main = keyLongMap;
+		key = inKey;
+	}
+
+	//--------------------------------------------------------------------------
+	//
+	// Incremental implementation
+	//
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Increment the value of the key and return the updated value.
+	 *
+	 * @param key to retrieve
+	 * @return Long
+	 */
+	public Long incrementAndGet(Object key) {
+		Long value = main.getValue(key);
+		value = new Long(value.longValue() + 1);
+		main.putValue((String) key, value);
+		return value;
+//		arithmeticLock.lock();
+//		Long returnValue = new Long(0);
+//		try{
+//			returnValue = main.getValue(key);
+//			long currentValue = returnValue.longValue() + 1;
+//			returnValue = new Long(currentValue);
+//			main.putValue((String)key, returnValue);
+//		}finally {
+//			arithmeticLock.unlock();
+//		}
+//		return returnValue;
+	}
+
+	/**
+	 * Return the current value of the key and increment by 1
+	 *
+	 * @param key to retrieve
+	 * @return Long
+	 */
+	public Long getAndIncrement(Object key){
+		Long value = main.getValue(key);
+		main.put((String) key, new Long(value.longValue() + 1));
+		return value;
+//		arithmeticLock.lock();
+//		Long returnValue = new Long(0);
+//		try{
+//			returnValue = main.getValue(key);
+//			long currentValue = returnValue.longValue() + 1;
+//			main.putValue((String)key, new Long(currentValue));
+//		}finally {
+//			arithmeticLock.unlock();
+//		}
+//		return returnValue;
+	}
+
+	/**
+	 * Decrement the value of the key and return the updated value.
+	 *
+	 * @param key to retrieve
+	 * @return Long
+	 */
+	public Long decrementAndGet(Object key){
+		Long value = main.getValue(key);
+		value = new Long(value.longValue() - 1);
+		main.putValue((String) key, value);
+		return value;
+	}
+
+	/**
+	 * Return the current value of the key and decrement by 1
+	 *
+	 * @param key to retrieve
+	 * @return Long
+	 */
+	public Long getAndDecrement(Object key){
+		Long value = main.getValue(key);
+		main.put((String) key, new Long(value.longValue() - 1));
+		return value;
+	}
+
+
+	//--------------------------------------------------------------------------
+	//
+	// Generic convert value implementation
+	//
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Update the currently stored value
+	 *
+	 * @param  value to update to
+	 **/
+	public void putValue(Long value) {
+		main.putValue(key, value);
+	}
+
+	/**
+	 * Remove the currently stored value
+	 **/
+	public void removeValue() {
+		main.remove(key);
+	}
+
+	/**
+	 * [Needs to be overriden, currently throws UnsupportedOperationException]
+	 *
+	 * Get and return its stored value.
+	 *
+	 * @return value used for generic convert where applicable
+	 **/
+	public Long getValue() {
+		return main.getValue(key);
+	}
+
+	/**
+	 * Default to String conversion of generic value
+	 *
+	 * @return The converted string, always possible unless null
+	 **/
+	@Override
+	public String toString() {
+		return getValue().toString();
+	}
+
+	//--------------------------------------------------------------------------
+	//
+	// Key name identifier handling
+	//
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Get and return the key used to store the value
+	 *
+	 * @return  the key representing the value
+	 */
+	public String getKey() {
+		return key;
+	}
+
+	//--------------------------------------------------------------------------
+	//
+	// Expiration and lifespan handling
+	//
+	//--------------------------------------------------------------------------
+
+	/**
+	 * Returns the expire time stamp value, if still valid
+	 *
+	 * @return long, 0 means no expirary, -1 no data / expired
+	 **/
+	public long getExpiry() {
+		return main.getExpiry(key);
+	}
+
+	/**
+	 * Returns the lifespan time stamp value
+	 *
+	 * @return long, 0 means no expirary, -1 no data / expired
+	 **/
+	public long getLifespan() {
+		return main.getLifespan(key);
+	}
+
+	/**
+	 * Sets the expire time stamp value, if still valid
+	 *
+	 * @param expireTimestamp expire unix timestamp value in milliseconds
+	 **/
+	public void setExpiry(long expireTimestamp) {
+		main.setExpiry(key, expireTimestamp);
+	}
+
+	/**
+	 * Sets the expire time stamp value, if still valid
+	 *
+	 * @param lifespan time to expire in milliseconds
+	 **/
+	public void setLifeSpan(long lifespan) {
+		main.setLifeSpan(key, lifespan);
+	}
+
+	/**
+	 * Stores (and overwrites if needed) key, value pair
+	 * with lifespan value.
+	 *
+	 * Important note: It does not return the previously stored value
+	 *
+	 * @param value as String
+	 * @param lifespan time to expire in milliseconds
+	 *
+	 * @return null
+	 **/
+	public Long putWithLifespan(Long value, long lifespan) {
+		return main.putWithLifespan(key, value, lifespan);
+	}
+
+	/**
+	 * Stores (and overwrites if needed) key, value pair
+	 * with expirary value.
+	 *
+	 * Important note: It does not return the previously stored value
+	 *
+	 * @param value as String
+	 * @param expireTimestamp expire unix timestamp value in milliseconds
+	 *
+	 * @return String
+	 **/
+	public Long putWithExpiry(Long value, long expireTimestamp) {
+		return main.putWithExpiry(key, value, expireTimestamp);
+	}
+}
