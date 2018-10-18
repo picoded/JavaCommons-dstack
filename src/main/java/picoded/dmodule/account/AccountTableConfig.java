b6fddbc63763fcc4fe49c3e36418fc41860f0086
jiaming <jiaming@uilicious.com>
2018-10-18 17:57:26 +0800

    remove the initialization of maps to null and add a note to check if removal of data is required in token and verification map

--- src/main/java/picoded/dmodule/account/AccountTableConfig.java
+++ src/main/java/picoded/dmodule/account/AccountTableConfig.java
@@ -14,18 +14,18 @@ import picoded.core.struct.template.UnsupportedDefaultMap;
  **/
 abstract class AccountTableConfig extends ModuleStructure implements
 	UnsupportedDefaultMap<String, AccountObject> {
-
+	
 	///////////////////////////////////////////////////////////////////////////
 	//
 	// Underlying data structures
 	//
 	///////////////////////////////////////////////////////////////////////////
-
+	
 	//
 	// Login authentication
 	//
 	//-----------------------------------------
-
+	
 	/**
 	 * Provides a key value pair mapping of the account login ID to AccountID (GUID)
 	 *
@@ -35,33 +35,33 @@ abstract class AccountTableConfig extends ModuleStructure implements
 	 * AccountID's are not unique, as a single AccountID can have multiple "names"
 	 **/
 	protected KeyValueMap accountLoginNameMap; //to delete from
-
+	
 	/**
 	 * Stores the account authentication hash, used for password based authentication
 	 *
 	 * KeyValueMap<AccountID,passwordHash>
 	 **/
 	protected KeyValueMap accountAuthMap; //to delete from
-
+	
 	//
 	// Login session
 	//
 	//-----------------------------------------
-
+	
 	/**
 	 * Stores the account session key, to accountID link
 	 *
 	 * KeyValueMap<sessionID, accountID>
 	 **/
 	protected KeyValueMap sessionLinkMap;
-
+	
 	/**
 	 * Stores the account token key, to session key
 	 *
 	 * KeyValueMap<tokenID, sessionID>
 	 **/
 	protected KeyValueMap sessionTokenMap;
-
+	
 	/**
 	 * Stores the next token ID to reissue
 	 * This limits race conditions where multiple tokens are issued
@@ -69,19 +69,19 @@ abstract class AccountTableConfig extends ModuleStructure implements
 	 * KeyValueMap<tokenID, next-tokenID>
 	 **/
 	protected KeyValueMap sessionNextTokenMap;
-
+	
 	/**
 	 * Stores the account meta information
 	 *
 	 * KeyValueMap<sessionID, info-about-access>
 	 **/
 	protected KeyValueMap sessionInfoMap;
-
+	
 	//
 	// Account meta information
 	//
 	//-----------------------------------------
-
+	
 	/**
 	 * Account meta information
 	 * Used to pretty much store all individual information
@@ -92,54 +92,54 @@ abstract class AccountTableConfig extends ModuleStructure implements
 	 * DataObjectMap<AccountOID, DataObject>
 	 **/
 	protected DataObjectMap accountDataObjectMap;
-
+	
 	//
 	// Login throttling information
 	//
 	//-----------------------------------------
-
+	
 	/**
 	 * Handles the Login Throttling Attempt Key (AccountID) Value (Attempt) field mapping
 	 *
 	 * KeyLongMap<UserOID, attempts>
 	 **/
 	protected KeyLongMap loginThrottlingAttemptMap;
-
+	
 	/**
 	 * Handles the Login Throttling Attempt Key (AccountID) Value (Timeout) field mapping
 	 *
 	 * KeyLongMap<UserOID, expireTimestamp>
 	 **/
 	protected KeyLongMap loginThrottlingExpiryMap;
-
+	
 	// //
 	// // Account Related Token Maps
 	// //
 	// //-----------------------------------------
-
+	
 	/**
 	 * Stores the verification token against the account ID together with the expiry time
 	 **/
-	protected KeyValueMap accountVerificationMap = null;
-
+	protected KeyValueMap accountVerificationMap;
+	
 	/**
 	 * Stores the verification token for account password resets with expiry time
 	 **/
-	protected KeyValueMap accountPasswordTokenMap = null;
-
+	protected KeyValueMap accountPasswordTokenMap;
+	
 	///////////////////////////////////////////////////////////////////////////
 	//
 	// Constructor setup : Setup the actual tables, with the various names
 	//
 	///////////////////////////////////////////////////////////////////////////
-
+	
 	/**
 	 * Setup with the given stack and name prefix for data structures
 	 **/
 	public AccountTableConfig(CommonStack inStack, String inName) {
 		super(inStack, inName);
 	}
-
+	
 	/**
 	 * Setup data structure from stack using default naming
 	 */
@@ -147,37 +147,37 @@ abstract class AccountTableConfig extends ModuleStructure implements
 		// Login auth information
 		accountLoginNameMap = stack.keyValueMap(name + "_ID");
 		accountAuthMap = stack.keyValueMap(name + "_IH");
-
+		
 		// Login session infromation
 		sessionLinkMap = stack.keyValueMap(name + "_SL");
 		sessionTokenMap = stack.keyValueMap(name + "_ST");
 		sessionNextTokenMap = stack.keyValueMap(name + "_SN");
 		sessionInfoMap = stack.keyValueMap(name + "_SI");
-
+		
 		// Account meta information
 		accountDataObjectMap = stack.dataObjectMap(name + "_AM");
-
+		
 		// Login throttling information
 		loginThrottlingAttemptMap = stack.keyLongMap(name + "_TA");
 		loginThrottlingExpiryMap = stack.keyLongMap(name + "_TE");
-
+		
 		// // Account Verification information
 		accountVerificationMap = stack.keyValueMap(name + "_AV");
 		// // Account Password Token information
 		accountPasswordTokenMap = stack.keyValueMap(name + "_AR");
-
+		
 		// Side note: For new table, edit here and add into the return List
 		// @TODO - Consider adding support for temporary tables typehints
 	}
-
+	
 	/**
 	 * Initialize the various internal data structures,
 	 * used by account from the stack.
 	 **/
 	protected List<CommonStructure> setupInternalStructureList() {
-
+		
 		setupDStackMaps();
-
+		
 		// Return it as a list
 		return Arrays.asList( //
 			accountLoginNameMap, accountAuthMap, //
@@ -187,91 +187,91 @@ abstract class AccountTableConfig extends ModuleStructure implements
 			accountVerificationMap, accountPasswordTokenMap //
 			);
 	}
-
+	
 	///////////////////////////////////////////////////////////////////////////
 	//
 	// DataObject sync settings
 	//
 	///////////////////////////////////////////////////////////////////////////
-
+	
 	/**
 	 * DataObject parameter name to sync login names into
 	 */
 	public String syncLoginNameList = "loginNameList";
-
+	
 	///////////////////////////////////////////////////////////////////////////
 	//
 	// Login session settings
 	//
 	///////////////////////////////////////////////////////////////////////////
-
+	
 	/**
 	 * New session lifespan without token
 	 **/
 	public int initSessionSetupLifespan = 30;
-
+	
 	/**
 	 * Race condition buffer for tokens
 	 **/
 	public int sessionRaceConditionBuffer = 10;
-
+	
 	/**
 	 * defined login lifetime, default as 3600 seconds (aka 1 hr)
 	 **/
 	public int loginLifetime = 3600; // 1 hr = 60 (mins) * 60 (seconds) = 3600 seconds
-
+	
 	/**
 	 * lifetime for http login token required for renewal, 1800 seconds (or half an hour)
 	 **/
 	public int loginRenewal = loginLifetime / 2; //
-
+	
 	/**
 	 * Remember me lifetime, default as 2592000 seconds (aka 30 days)
 	 **/
 	public int rememberMeLifetime = 2592000; // 1 mth ~= 30 (days) * 24 (hrs) * 3600 (seconds in an hr)
-
+	
 	/**
 	 * Remember me lifetime, default the same as loginRenewal
 	 **/
 	public int rememberMeRenewal = loginRenewal;
-
+	
 	/**
 	 * Sets the cookie to be limited to http only
 	 **/
 	public boolean isHttpOnly = false;
-
+	
 	/**
 	 * Sets the cookie to be via https only
 	 **/
 	public boolean isSecureOnly = false;
-
+	
 	/**
 	 * Sets the cookie namespace prefix
 	 **/
 	public String cookiePrefix = "account_";
-
+	
 	/**
 	 * Sets teh cookie domain, defaults is null
 	 **/
 	public String cookieDomain = null;
-
+	
 	/**
 	 * The nonce size
 	 **/
 	public int nonceSize = 22;
-
+	
 	/**
 	 * Cookie path settings to overwrite, use NULL to use contextPath (as detected)
 	 **/
 	public String cookiePath = null;
-
+	
 	// /**
 	//  * Load and configures the session config from the given map
 	//  */
 	// public void loadAuthConfig(Map<String,Object> inConfig) {
 	// 	//@TODO : implement config loading
 	// }
-
+	
 	/**
 	 * Utility function to get the configured cookie lifetime, with the relevent settings
 	 *
@@ -286,13 +286,13 @@ abstract class AccountTableConfig extends ModuleStructure implements
 			return loginLifetime;
 		}
 	}
-
+	
 	///////////////////////////////////////////////////////////////////////////
 	//
 	// Login throttling configuration
 	//
 	///////////////////////////////////////////////////////////////////////////
-
+	
 	/**
 	 * Login throttling lambda function, which can be overwritten for
 	 * custom login throttling requirements
@@ -313,13 +313,13 @@ abstract class AccountTableConfig extends ModuleStructure implements
 		// 7     - 15
 		return (long) (Math.pow(2, Math.max(0, attempts - 3)) - 1);
 	};
-
+	
 	///////////////////////////////////////////////////////////////////////////
 	//
 	// UnsupportedDefaultMap compliance
 	//
 	///////////////////////////////////////////////////////////////////////////
-
+	
 	/**
 	 * Removes all data, without tearing down setup
 	 * (Reimplemented to work around interface conflict)
@@ -329,5 +329,5 @@ abstract class AccountTableConfig extends ModuleStructure implements
 	public void clear() {
 		systemSetupInterfaceCollection().forEach(item -> item.clear());
 	}
-
+	
 }
