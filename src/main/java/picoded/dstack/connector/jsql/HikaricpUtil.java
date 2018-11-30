3330d7ae1651ce38f9912cdfbe3fa24869ac40be
Eugene Cheah <eugeneqin@gmail.com>
2018-11-30 10:08:50 +0800

    WIP JSql refactor

--- src/main/java/picoded/dstack/connector/jsql/HikaricpUtil.java
+++ src/main/java/picoded/dstack/connector/jsql/HikaricpUtil.java
@@ -1,29 +1,91 @@
 package picoded.dstack.connector.jsql;
 
+import com.zaxxer.hikari.*;
+import picoded.core.struct.*;
+
 /**
  * HikariCP utility class 
  **/
 class HikaricpUtil {
-
+	
+	//
+	// Configuration loading from map
+	//
+	
+	/**
+	 * Loading the common HikariConfig settings - given a config map.
+	 * 
+	 * This exclude database specific configuration loading
+	 * 
+	 * @param  config map used
+	 **/
+	public static HikariConfig commonConfigLoading(GenericConvertMap config) {
+		// Fallback to a blank map (if needed)
+		if (config == null) {
+			config = new GenericConvertHashMap<>();
+		}
+		
+		// config object
+		HikariConfig ret = new HikariConfig();
+		
+		//
+		// Setup the various individual config, with default fallback
+		//
+		// For additional config to add support in future (if needed) see
+		// https://github.com/brettwooldridge/HikariCP
+		//
+		
+		// autoCommit
+		ret.setAutoCommit(config.getBoolean("autoCommit", ret.isAutoCommit()));
+		
+		// connectionTimeout
+		ret.setConnectionTimeout(config.getLong("connectionTimeout", ret.getConnectionTimeout()));
+		
+		// idleTimeout
+		ret.setIdleTimeout(config.getLong("idleTimeout", ret.getIdleTimeout()));
+		
+		// maxLifetime
+		ret.setMaxLifetime(config.getLong("maxLifetime", ret.getMaxLifetime()));
+		
+		// connectionTestQuery
+		// **not supported:** we enforce JDBC4 and above drivers (for now)
+		
+		// maximumPoolSize
+		ret.setMaximumPoolSize(config.getInt("maximumPoolSize", defaultMaxPoolSize()));
+		
+		// minimumIdle
+		ret.setMinimumIdle(config.getInt("minimumIdle", 2));
+		
+		// Return config object
+		return ret;
+	}
+	
+	/**
+	 */
+	
+	//
+	// Default configuration handling
+	//
+	
 	// static memoizer for the default max pool size
 	static int _defaultMaxPoolSize = -1;
-
+	
 	/**
-	 * Default max pool size is always atleast 2
+	 * Default max pool size is always atleast 4
 	 * 
 	 * Default max pool size will be tagged to the total avaliableProcessors count.
 	 * Which would 2 * number of cores, for a hyper threading environment.
 	 * 
-	 * Coinceidentally, this is the optimal guideline for max coonection pool sizing
+	 * This is close to the optimal guideline for max coonection pool sizing
 	 * See : https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
 	 * 
 	 * If hyperthreading is disabled, this estimate is ineffective. But is considered out of scope.
 	 * 
 	 * @return default max pool size to use
-	 */
-	static int defaultMaxPoolSize() {
+	 **/
+	public static int defaultMaxPoolSize() {
 		// Current pool size has not been calculated - calculate it
-		if( _defaultMaxPoolSize <= 0 ) {
+		if (_defaultMaxPoolSize <= 0) {
 			_defaultMaxPoolSize = Math.max(2, Runtime.getRuntime().availableProcessors());
 		}
 		return _defaultMaxPoolSize;
