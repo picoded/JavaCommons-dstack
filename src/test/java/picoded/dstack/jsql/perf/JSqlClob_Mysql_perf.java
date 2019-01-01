5a7a094fe5c960a467935ee96a9193e65d31b357
Eugene Cheah <eugeneqin@gmail.com>
2019-01-01 15:11:04 +0800

    Commenting out performance based test cases of old JSql (before hikariCP refactor)
    They can be restored back, and refactored when needed

--- src/test/java/picoded/dstack/jsql/perf/JSqlClob_Mysql_perf.java
+++ src/test/java/picoded/dstack/jsql/perf/JSqlClob_Mysql_perf.java
@@ -1,18 +1,18 @@
-package picoded.dstack.jsql.perf;
+// package picoded.dstack.jsql.perf;
 
-import picoded.dstack.jsql.connector.*;
-import picoded.dstack.jsql.*;
-import picoded.dstack.jsql.JSqlTestConfig;
+// import picoded.dstack.jsql.connector.*;
+// import picoded.dstack.jsql.*;
+// import picoded.dstack.jsql.JSqlTestConfig;
 
-/// [Mysql varient]
-/// Testing of DataTable full CLOB structure performance
-public class JSqlClob_Mysql_perf extends JSqlClob_perf {
-	
-	/// Note that this SQL connector constructor
-	/// is to be overriden for the various backend
-	/// specific test cases
-	public JSql jsqlConnection() {
-		return JSqlTestConnection.mysql();
-	}
-	
-}
+// /// [Mysql varient]
+// /// Testing of DataTable full CLOB structure performance
+// public class JSqlClob_Mysql_perf extends JSqlClob_perf {
+
+// 	/// Note that this SQL connector constructor
+// 	/// is to be overriden for the various backend
+// 	/// specific test cases
+// 	public JSql jsqlConnection() {
+// 		return JSqlTestConnection.mysql();
+// 	}
+
+// }
--- src/test/java/picoded/dstack/jsql/perf/JSqlObjKeyValBatchless_perf.java
+++ src/test/java/picoded/dstack/jsql/perf/JSqlObjKeyValBatchless_perf.java
@@ -1,38 +1,38 @@
-package picoded.dstack.jsql.perf;
+// package picoded.dstack.jsql.perf;
 
-import java.util.*;
-import picoded.dstack.jsql.connector.*;
-import picoded.dstack.jsql.*;
-import picoded.dstack.jsql.JSqlTestConfig;
+// import java.util.*;
+// import picoded.dstack.jsql.connector.*;
+// import picoded.dstack.jsql.*;
+// import picoded.dstack.jsql.JSqlTestConfig;
 
-/// Testing of DataTable obje-key-val, in batchless mode
-public class JSqlObjKeyValBatchless_perf extends JSqlObjKeyVal_perf {
-	
-	/// Does a delta update of changes, with a GUID and original object for refrence
-	public void update(String guid, Map<String, Object> ori, Map<String, Object> delta) {
-		// Delta keys to iterate
-		String[] deltaKeys = delta.keySet().toArray(new String[0]);
-		
-		// For each key, upsert
-		for (int i = 0; i < deltaKeys.length; ++i) {
-			// attribute by attribute upsert
-			jsqlObj.upsert( //
-				tablename, // Table name to upsert on
-				//
-				new String[] { "oID", "kID" }, new Object[] { guid, deltaKeys[i] }, // The row unique identifier values
-				//
-				new String[] { "sVl" }, // Columns names to update
-				new Object[] { delta.get(deltaKeys[i]) }, // Values to update
-				// Columns names to apply default value, if not exists
-				// Values to insert, that is not updated. Note that this is ignored if pre-existing values exists
-				null, //
-				null, //
-				// Various column names where its existing value needs to be maintained (if any),
-				// this is important as some SQL implementation will fallback to default table values, if not properly handled
-				null //
-				);
-		}
-		
-		return;
-	}
-}
+// /// Testing of DataTable obje-key-val, in batchless mode
+// public class JSqlObjKeyValBatchless_perf extends JSqlObjKeyVal_perf {
+
+// 	/// Does a delta update of changes, with a GUID and original object for refrence
+// 	public void update(String guid, Map<String, Object> ori, Map<String, Object> delta) {
+// 		// Delta keys to iterate
+// 		String[] deltaKeys = delta.keySet().toArray(new String[0]);
+
+// 		// For each key, upsert
+// 		for (int i = 0; i < deltaKeys.length; ++i) {
+// 			// attribute by attribute upsert
+// 			jsqlObj.upsert( //
+// 				tablename, // Table name to upsert on
+// 				//
+// 				new String[] { "oID", "kID" }, new Object[] { guid, deltaKeys[i] }, // The row unique identifier values
+// 				//
+// 				new String[] { "sVl" }, // Columns names to update
+// 				new Object[] { delta.get(deltaKeys[i]) }, // Values to update
+// 				// Columns names to apply default value, if not exists
+// 				// Values to insert, that is not updated. Note that this is ignored if pre-existing values exists
+// 				null, //
+// 				null, //
+// 				// Various column names where its existing value needs to be maintained (if any),
+// 				// this is important as some SQL implementation will fallback to default table values, if not properly handled
+// 				null //
+// 				);
+// 		}
+
+// 		return;
+// 	}
+// }
