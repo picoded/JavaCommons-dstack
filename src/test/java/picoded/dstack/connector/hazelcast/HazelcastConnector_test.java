7fe7adeeec20dc45659375c72fe2fe97642ed45a
Eugene Cheah <eugeneqin@gmail.com>
2019-07-27 07:33:50 +0000

    ✅ Hazelcast client mode testing

--- src/test/java/picoded/dstack/connector/hazelcast/HazelcastConnector_test.java
+++ src/test/java/picoded/dstack/connector/hazelcast/HazelcastConnector_test.java
@@ -3,6 +3,7 @@ package picoded.dstack.connector.hazelcast;
 import java.util.*;
 
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.Member;
 
 // junit include
 import org.junit.*;
@@ -18,14 +19,56 @@ import static org.junit.Assert.*;
 public class HazelcastConnector_test {
 	
 	@Test
-	public void sanityTest() throws Exception {
+	public void serverTest() throws Exception {
 		// Config map
 		GenericConvertMap<String, Object> config = new GenericConvertHashMap<>();
-		config.put("groupName", "SanityTest");
+		config.put("groupName", "HazelcastServerConnectorTest");
 		
 		// Server instance
 		HazelcastInstance instance = HazelcastConnector.getConnection(config);
 		assertNotNull(instance);
+		
+		// Close it up
 		HazelcastConnector.closeConnection(instance);
+	}
+	
+	@Test
+	public void serverClientTest() throws Exception {
+		// Config map
+		GenericConvertMap<String, Object> config = new GenericConvertHashMap<>();
+		config.put("groupName", "HazelcastClientConnectorTest");
+		
+		// Server instance
+		HazelcastInstance server = HazelcastConnector.getConnection(config);
+		assertNotNull(server);
+		
+		// Lets store some data
+		Map<String, String> store = server.getMap("hello");
+		store.put("world", "one");
+		
+		// Lets get server info
+		Member mem = server.getCluster().getLocalMember();
+		assertNotNull(mem);
+		
+		// Lets get server info
+		int port = mem.getAddress().getPort();
+		
+		// Lets setup client
+		GenericConvertMap<String, Object> clientConfig = new GenericConvertHashMap<>();
+		clientConfig.put("groupName", "HazelcastClientConnectorTest");
+		clientConfig.put("mode", "client");
+		clientConfig.put("memberTcpList", "[\"localhost:" + port + "\"]");
+		
+		// Lets load up the client
+		HazelcastInstance client = HazelcastConnector.getConnection(clientConfig);
+		assertNotNull(client);
+		
+		// Lets assert client data
+		Map<String, String> read = client.getMap("hello");
+		assertEquals("one", read.get("world"));
+		
+		// Close it up
+		HazelcastConnector.closeConnection(client);
+		HazelcastConnector.closeConnection(server);
 	}
 }
