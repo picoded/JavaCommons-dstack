645252bc99e3cd64f5b7e3cadba78a3e83848048
Eugene Cheah <eugeneqin@gmail.com>
2019-12-30 15:05:43 +0000

    init submodules as part of gradle action

---
+++ git-init-submodules.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This script is here, so that i will not need to keep googling
+# how to do this essential command, of preloading all the submodules
+git submodule update --init --recursive
