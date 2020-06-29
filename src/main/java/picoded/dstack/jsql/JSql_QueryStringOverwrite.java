50e05776be3834b42eae3ddeea1c1bdfb8c702dc
Eugene Cheah <eugeneqin@gmail.com>
2020-06-29 06:55:11 +0000

    src beautify querystringoverwrite

--- src/main/java/picoded/dstack/jsql/JSql_QueryStringOverwrite.java
+++ src/main/java/picoded/dstack/jsql/JSql_QueryStringOverwrite.java
@@ -35,98 +35,98 @@ import picoded.core.struct.MutablePair;
  * needing to be rewritten for specific use cases
  */
 class JSql_QueryStringOverwrite implements Query {
-		
-    /**
-     * The original query used internally
-     */
-    Query original = null;
-    
-    /**
-     * Overwrite SQL string to expose
-     */
-    String toStringOverwrite = null;
-    
-    /**
-     * Constructor, with the respective query, and SQL string overwrite
-     */
-    public JSql_QueryStringOverwrite(Query inOriginal, String inOverwrite) {
-        original = inOriginal;
-        toStringOverwrite = inOverwrite;
-    }
-    
-    //
-    // Proxy the respective compulsory command to the original query
-    //
-    @Override
-    public boolean test(Object t) {
-        return original.test(t);
-    }
-    
-    @Override
-    public boolean test(Object t, Map<String, Object> argMap) {
-        return original.test(t, argMap);
-    }
-    
-    @Override
-    public QueryType type() {
-        return original.type();
-    }
-    
-    @Override
-    public String operatorSymbol() {
-        return original.operatorSymbol();
-    }
-    
-    @Override
-    public Map<String, List<Object>> keyValuesMap(Map<String, List<Object>> mapToReturn) {
-        return original.keyValuesMap(mapToReturn);
-    }
-    
-    //
-    // Operator type overwrites
-    //
-    
-    @Override
-    public boolean isBasicOperator() {
-        return original.isBasicOperator();
-    }
-    
-    @Override
-    public boolean isCombinationOperator() {
-        return original.isCombinationOperator();
-    }
-    
-    //
-    // Non compulsory overwrites
-    //
-    
-    public List<Query> childrenQuery() {
-        return original.childrenQuery();
-    }
-    
-    public String fieldName() {
-        return original.fieldName();
-    }
-    
-    public String argumentName() {
-        return original.argumentName();
-    }
-    
-    public Map<String, Object> defaultArgumentMap() {
-        return original.defaultArgumentMap();
-    }
-    
-    public Object defaultArgumentValue() {
-        return original.defaultArgumentValue();
-    }
-    
-    //
-    // toString() overwrite, this also effectively overwrites the toSqlString() equilavent
-    //
-    
-    @Override
-    public String toString() {
-        return toStringOverwrite;
-    }
-    
+	
+	/**
+	 * The original query used internally
+	 */
+	Query original = null;
+	
+	/**
+	 * Overwrite SQL string to expose
+	 */
+	String toStringOverwrite = null;
+	
+	/**
+	 * Constructor, with the respective query, and SQL string overwrite
+	 */
+	public JSql_QueryStringOverwrite(Query inOriginal, String inOverwrite) {
+		original = inOriginal;
+		toStringOverwrite = inOverwrite;
+	}
+	
+	//
+	// Proxy the respective compulsory command to the original query
+	//
+	@Override
+	public boolean test(Object t) {
+		return original.test(t);
+	}
+	
+	@Override
+	public boolean test(Object t, Map<String, Object> argMap) {
+		return original.test(t, argMap);
+	}
+	
+	@Override
+	public QueryType type() {
+		return original.type();
+	}
+	
+	@Override
+	public String operatorSymbol() {
+		return original.operatorSymbol();
+	}
+	
+	@Override
+	public Map<String, List<Object>> keyValuesMap(Map<String, List<Object>> mapToReturn) {
+		return original.keyValuesMap(mapToReturn);
+	}
+	
+	//
+	// Operator type overwrites
+	//
+	
+	@Override
+	public boolean isBasicOperator() {
+		return original.isBasicOperator();
+	}
+	
+	@Override
+	public boolean isCombinationOperator() {
+		return original.isCombinationOperator();
+	}
+	
+	//
+	// Non compulsory overwrites
+	//
+	
+	public List<Query> childrenQuery() {
+		return original.childrenQuery();
+	}
+	
+	public String fieldName() {
+		return original.fieldName();
+	}
+	
+	public String argumentName() {
+		return original.argumentName();
+	}
+	
+	public Map<String, Object> defaultArgumentMap() {
+		return original.defaultArgumentMap();
+	}
+	
+	public Object defaultArgumentValue() {
+		return original.defaultArgumentValue();
+	}
+	
+	//
+	// toString() overwrite, this also effectively overwrites the toSqlString() equilavent
+	//
+	
+	@Override
+	public String toString() {
+		return toStringOverwrite;
+	}
+	
 }
