7f6de6fda5c44c0153c3b15f48b3e26f400dd599
Eugene Cheah <eugeneqin@gmail.com>
2021-11-30 09:07:10 +0000

    DataObjectMap perf tests

---
+++ src/test/java/picoded/dstack/jsql/postgres/JSql_DataObjectMap_Postgres_perf.java
@@ -0,0 +1,40 @@
+package picoded.dstack.jsql.postgres;
+
+// Target test class
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+// Test Case include
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+// Test depends
+import picoded.dstack.*;
+import picoded.dstack.connector.jsql.*;
+import picoded.dstack.struct.simple.*;
+
+public class JSql_DataObjectMap_Postgres_perf extends StructSimple_DataObjectMap_perf {
+	
+	// To override for implementation
+	//-----------------------------------------------------
+	
+	/// Note that this SQL connector constructor
+	/// is to be overriden for the various backend
+	/// specific test cases
+	public JSql jsqlConnection() {
+		return JSqlTestConnection.postgres();
+	}
+	
+	/// Impomentation constructor for SQL
+	public DataObjectMap implementationConstructor() {
+		return new JSql_DataObjectMap(jsqlConnection(), JSqlTestConfig.randomTablePrefix());
+	}
+	
+}
