8a493616740f5acbfa0c8834478b71f2e9b5bf74
remy <remy@uilicious.com>
2022-08-02 22:26:26 +0800

    Redis_DataObjectMap

--- src/main/java/picoded/dstack/redis/Redis_DataObjectMap.java
+++ src/main/java/picoded/dstack/redis/Redis_DataObjectMap.java
@@ -22,6 +22,7 @@ import picoded.core.struct.query.Query;
 import picoded.core.common.ObjectToken;
 import picoded.dstack.*;
 import picoded.dstack.core.*;
+import picoded.core.struct.query.*;
 
 // Redis imports
 import org.redisson.Redisson;
@@ -156,6 +157,21 @@ public class Redis_DataObjectMap extends Core_DataObjectMap_struct {
 	public void systemDestroy() {
 		redisMap.delete();
 	}
+
+	/**
+	 * [Internal use, to be extended in future implementation]
+	 *
+	 * Removes the complete remote data map, for DataObject.
+	 * This is used to nuke an entire object
+	 *
+	 * @param  Object ID to remove
+	 *
+	 * @return  nothing
+	 **/
+	public void DataObjectRemoteDataMap_remove(String _oid) {
+		// //redisson.getKeys().delete(_oid);
+		redisMap.fastRemove(_oid);
+	}
 	
 	/**
 	 * Updates the actual backend storage of DataObject
@@ -170,12 +186,6 @@ public class Redis_DataObjectMap extends Core_DataObjectMap_struct {
 		for (String key : fullMap.keySet()) {
 			// Get the full map value
 			Object val = fullMap.get(key);
-
-			// // Check for Map / List like objects
-			// if (val instanceof Map || val instanceof List) {
-			// 	// Clone it - by JSON serializing back and forth
-			// 	clonedMap.put(key, ConvertJSON.toObject(ConvertJSON.fromObject(val)));
-			// } 
 
 			if (updateKeys.contains(key)) {
 				clonedMap.put(key, val);
@@ -235,127 +245,18 @@ public class Redis_DataObjectMap extends Core_DataObjectMap_struct {
 		return ret;
 	}
 	
-	// /**
-	//  * @return set of keys
-	//  **/
-	// @Override
-	// public Set<String> keySet() {
-	// 	// The return hashset
-	// 	HashSet<String> ret = new HashSet<String>();
-	
-	// 	//Fetch everything in current db
-	// 	RKeys keySet = redisson.getKeys();
-	// 	if (value != null) {
-	// 		// Return key where value is matched
-	// 		keySet.getKeysByPattern(value).forEach(k -> ret.add(k));
-	// 	} else {
-	// 		// Return the full keyset
-	// 		keySet.getKeys().forEach(k -> ret.add(k));
-	// 	}
-	// 	return ret;
-	// }
-	
 	/**
-	 * [Internal use, to be extended in future implementation]
-	 *
-	 * Removes the complete remote data map, for DataObject.
-	 * This is used to nuke an entire object
-	 *
-	 * @param  Object ID to remove
-	 *
-	 * @return  nothing
+	 * @return set of keys
 	 **/
-	public void DataObjectRemoteDataMap_remove(String _oid) {
-		// //redisson.getKeys().delete(_oid);
-		redisMap.fastRemove(_oid);
-	}
-
-	/**
-	 * Given the SQL style query, convert it to relevant Redis Object
-	 */
-	static protected void queryToRedis(Query inQuery) {
-		QueryType type = inQuery.type();
-		
-		// Handle the query according to its type
-		// Basic operator
-		if (type == QueryType.EQUALS) {
-			//TODO
-		}
-		if (type == QueryType.NOT_EQUALS) {
-			//TODO
-		}
-		if (type == QueryType.LESS_THAN) {
-			//TODO
-		}
-		if (type == QueryType.LESS_THAN_OR_EQUALS) {
-			//TODO
-		}
-		if (type == QueryType.MORE_THAN) {
-			//TODO
-		}
-		if (type == QueryType.MORE_THAN_OR_EQUALS) {
-			//TODO
-		}
-		if (type == QueryType.LIKE) {
-			//TODO
-		}
-		throw new RuntimeException("Unkown query type : " + inQuery.type());
-	}
-	
-	/**
-	 * Performs a search query, and returns the respective DataObject keys.
-	 *
-	 * This is the GUID key varient of query, this is critical for stack lookup
-	 *
-	 * @param   queryClause, of where query statement and value
-	 * @param   orderByStr string to sort the order by, use null to ignore
-	 * @param   offset of the result to display, use -1 to ignore
-	 * @param   number of objects to return max, use -1 to ignore
-	 *
-	 * @return  The String[] array
-	 **/
-	public String[] query_id(Query queryClause, String orderByStr, int offset, int limit) {
-	
-		System.out.println("QUERY: "+queryClause);
-		// The return list of DataObjects
+	@Override
+	public Set<String> keySet() {
+		// The return hashset
+		HashSet<String> ret = new HashSet<String>();
+		//Fetch everything in current db
 		List<String> retList = null;
-	
-		RMap<String, Object> myRedisMap = redisMap;
-	
-		// Setup the query, if needed
-		if (queryClause == null) {
-			// Null gets all
-			retList = new ArrayList<String>(myRedisMap.readAllKeySet());
-		} else {
-	
-			// Get the list of _oid that passes the query
-			//Set<String> idSet = backendIMap().keySet(queryPredicate);
-			//String[] idArr = idSet.toArray(new String[0]);
-	
-			// DataObject[] from idArr
-			//DataObject[] doArr = getArrayFromID(idArr, true);
-	
-			// Converts to a list
-			//retList = new ArrayList(Arrays.asList(doArr));
-			retList = new ArrayList<String>(myRedisMap.readAllKeySet());
-		}
-
-		System.out.println("QueryResult: "+retList);
-	
-		// Sort, offset, convert to array, and return
-		// ???
-	
-		// Prepare the actual return string array
-		int retLength = retList.size();
-		String[] ret = new String[retLength];
-		for (int a = 0; a < retLength; ++a) {
-			//._oid(); -> where is it coming from
-			//ret[a] = retList.get(a)._oid();
-			ret[a] = String.valueOf(retList.get(a));
-		}
-	
-		System.out.println("Return: "+Arrays.toString(ret));
-		// Returns sorted array of strings
+		retList = new ArrayList<String>(redisMap.readAllKeySet());
+		// Return the full keyset
+		retList.forEach(k -> ret.add(k));
 		return ret;
 	}
 	
