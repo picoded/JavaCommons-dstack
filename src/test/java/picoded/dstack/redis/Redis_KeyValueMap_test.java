ba5858da6960b26e62926ee51c1e3f547b58774f
rplauta <rplauta@gmail.com>
2022-08-03 18:07:55 +0800

    Create Redis_KeyValueMap_test.java

---
+++ src/test/java/picoded/dstack/redis/Redis_KeyValueMap_test.java
@@ -0,0 +1,40 @@
+package picoded.dstack.redis;
+
+import picoded.core.struct.*;
+import picoded.dstack.*;
+import picoded.dstack.struct.simple.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class RedisKeyValueMap_test extends StructSimple_KeyValueMap_test {
+	// Redis stack instance
+	protected static volatile RedisStack instance = null;
+	
+	/// Implementation constructor
+	public KeyValueMap implementationConstructor() {
+		
+		// Initialize server
+		synchronized (RedisKeyValueMap_test.class) {
+			if (instance == null) {
+				// The default config uses "172.17.0.1" (default docker bridge address) 
+				// and port 6379 (default redis port)
+				GenericConvertMap<String, Object> redisConfig = new GenericConvertHashMap<>();
+				redisConfig.put("host", DStackTestConfig.REDIS_HOST());
+				redisConfig.put("port", DStackTestConfig.REDIS_PORT());
+				
+				// Use a random DB number between 0 and 15.
+				// int randomNum = ThreadLocalRandom.current().nextInt(0, 15 + 1);
+				redisConfig.put("name", DStackTestConfig.randomTablePrefix());
+				
+				GenericConvertMap<String, Object> stackConfig = new GenericConvertHashMap<>();
+				stackConfig.put("name", "RedisKeyValueMap_test");
+				stackConfig.put("redis", redisConfig);
+				
+				instance = new RedisStack(stackConfig);
+			}
+		}
+		// Load the KeyValueMap
+		return instance.keyValueMap(DStackTestConfig.randomTablePrefix());
+	}
+}
