67257cd981a44acbde8c25ab77507682a8049b97
Eugene Cheah <eugeneqin@gmail.com>
2022-09-13 05:53:17 +0000

    Seperated LIKE with LIKEORALL option

--- src/main/java/picoded/dstack/mongodb/MongoDB_DataObjectMap.java
+++ src/main/java/picoded/dstack/mongodb/MongoDB_DataObjectMap.java
@@ -436,6 +436,11 @@ public class MongoDB_DataObjectMap extends Core_DataObjectMap {
 			search = collection.find(bsonFilter);
 		} else if (sec_mode.equals("LIKE")) {
 			// Use secondary connection for LIKE query
+			if (queryClause != null && queryClause.toSqlString().toUpperCase().indexOf("LIKE") > 0) {
+				search = sec_collection.find(bsonFilter);
+			}
+		} else if (sec_mode.equals("LIKEORALL")) {
+			// Use secondary connection for LIKE query
 			if (queryClause == null || queryClause.toSqlString().toUpperCase().indexOf("LIKE") > 0) {
 				search = sec_collection.find(bsonFilter);
 			}
